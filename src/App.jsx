a231e1424219c28e0f9b646fc236b96e080b592a
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-01-14 21:55:19 +0100

    Update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import Navigation from './components/Navigation'
 import Hero from './components/Hero'
-import HeroGuides from './components/HeroGuides'
-import HeroBuildCalculator from './components/HeroBuildCalculator'
-import About from './components/About'
+import Features from './components/Features'
+import HeroGuide from './components/HeroGuide'
+import Reviews from './components/Reviews'
 import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="bg-dota-dark min-h-screen">
+    <div className="bg-[#0F172A] min-h-screen">
       <Navigation />
       <Hero />
-      <HeroGuides />
-      <HeroBuildCalculator />
-      <About />
+      <Features />
+      <HeroGuide />
+      <Reviews />
       <Footer />
     </div>
   )
